bac5023f4db3ba502ced62d4188dd44c7d5e4822
Droldg <daro23ac@student.cbs.dk>
2024-12-12 18:22:42 +0100

    File limit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(
 );
 
 app.use(express.json({ limit: '10mb' })); // Tillader op til 10 MB JSON
-app.use(express.urlencoded({ limit: '10mb', extended: true })); // Tillader op til 10 MB form-data
+app.use(express.urlencoded({ limit: '100mb', extended: true })); // Tillader op til 10 MB form-data
 
 app.use(cookieParser());
 
